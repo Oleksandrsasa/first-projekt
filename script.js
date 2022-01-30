ee9f844afd2cdb6e50d4981945b0a5311194d844
Oleksandr <example@gmail.com>
2022-01-30 19:13:45 +0200

    nothing

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+// console.log('arr' + " - object");
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 2);
+
+// console.log(2 + 2 * 2 !== '6');
+
+
+
+// const isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose);
+// alert('Hello');
+// const result = confirm("як справи?");
+// console.log(result);
+// const answer = +prompt("Ви курите?", "ні")
+// console.log(answer + 5);
+
+// const obj = {
+//     name: "john",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age) то буде 25
+// const answers = [];
+
+//  answers[0] = prompt('як тебе звати?', '');
+//  answers[1] = prompt('як тебе по батьково?', '');
+//  answers[2] = prompt('скольки в тебе свиней?', '');
+
+// console.log(typeof (answers));
+// console.log(typeof (null));
